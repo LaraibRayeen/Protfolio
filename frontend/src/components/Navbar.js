274f1a10d1bf4c6f694027a8548577cc79e9e66e
Laraib <laraibrayeen123@gmail.com>
2022-09-24 23:33:21 +0530

    ..

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
     <>
      
      <nav className="navbar navbar-expand-lg fixed-top">
-  <div className="container-fluid">
+  <div className="container">
     <a className="navbar-brand" href="#">
-      <img src={logo} className="img-fluid"></img>
+      <img src={logo} className="img-fluid w-75 h-auto d-inline-block"></img>
       
     </a>
     <button
@@ -24,32 +24,32 @@ const Navbar = () => {
     </button>
     <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
       <ul className="navbar-nav">
-        <li className="nav-item">
+        <li className="nav-item my-0  mx-2">
           <a className="nav-link active" aria-current="page" href="#Home">
             Home
           </a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#about">
+        <li className="nav-item my-0  mx-2">
+          <a className="nav-link " href="#about">
             About
           </a>
         </li>
-        <li className="nav-item">
+        <li className="nav-item my-0  mx-2">
           <a className="nav-link" href="#services">
             Services
           </a>
         </li>
-        <li className="nav-item">
+        <li className="nav-item my-0  mx-2">
           <a className="nav-link" href="#portfolio">
             Portfolio
           </a>
         </li>
-        <li className="nav-item">
+        <li className="nav-item my-0  mx-2">
           <a className="nav-link" href="#contect">
             Contect
           </a>
         </li>
-        <li className="nav-item mt-3 mt-lg-0">
+        <li className="nav-item mt-3 mt-lg-0 my-0  mx-5">
           <a  type="button" className="btn btn-light btn-outline-dark" data-mdb-ripple-color="dark" href="#">
             Hire me
           </a>
