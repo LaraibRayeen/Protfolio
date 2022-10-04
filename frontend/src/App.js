180aa48e11fd825656fe00cde029594a364337da
Laraib Rayeen <laraibrayeen123@gmail.com>
2022-10-04 17:45:52 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Banner from './components/Banner'
 import About from './components/About'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+// import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import Project from './components/Project';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 function App() {
   return (
   <>
-  <BrowserRouter> 
+  
   
   <Navbar/>
   <Banner/>
@@ -19,14 +19,6 @@ function App() {
   <Contact/>
   <Footer/>
   
-  <Routes>
-
-    <Route element={<About/>} path="About"/>
-    <Route element={<Banner/>} path="Banner"/>
-    
-  
-  </Routes>
-  </BrowserRouter>
 
   </>
   );
