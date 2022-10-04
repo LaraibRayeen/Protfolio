7efc721d213a7483c9682cc85db69282f81083b9
Laraib Rayeen <laraibrayeen123@gmail.com>
2022-10-04 18:06:03 +0530

    .

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./Navbar.css";
 import logo from "../Image/laraibLogo.jpg";
 const Navbar = () => {
-  let navBar = document.querySelectorAll(".nav-link");
-  let navCollapse = document.querySelector(".navbar-collapse.collapse");
-  navBar.forEach(function (a){
-      a.addEventListener("click", function(){
-          navCollapse.classList.remove("show");
-      })
-  })
+ 
    
+  let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+ 
   return (
     <>
       <nav className="navbar navbar-expand-lg fixed-top mb-5  bg-light">
